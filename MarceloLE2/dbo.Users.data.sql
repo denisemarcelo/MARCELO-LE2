7aafe71739f6a7988a218d16dd0de6c6f7cfa491
unknown <denisenmarcelo@gmail.com>
2023-03-02 01:56:13 +0800

    LE2-MARCELO

---
+++ MarceloLE2/dbo.Users.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Users] ON
+INSERT INTO [dbo].[Users] ([Id], [UserName], [FirstName], [LastName], [Password]) VALUES (1, N'dnbmarcelo', N'Denise Nicole', N'Marcelo', N'2019135392')
+SET IDENTITY_INSERT [dbo].[Users] OFF
